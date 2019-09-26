446c454fbe3c35f2f8d417bfbb39015ce2731821
toly <1981462002@qq.com>
2019-09-26 10:53:55 +0800

    初始项目.

---
+++ src/main/java/com/toly1994/toly_sb/TolySbApplication.java
@@ -0,0 +1,13 @@
+package com.toly1994.toly_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolySbApplication {
+
+    public static void main(String[] args) {//程序入口
+        SpringApplication.run(TolySbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toly1994/toly_sb/TolySbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toly1994.toly_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolySbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
